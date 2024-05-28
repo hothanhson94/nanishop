a991ba9f07271c2378aadf390ceab24a45e8fd3c
hothanhson94 <hothanhson1111@gmail.com>
2024-05-29 01:41:34 +0700

    Modified settings.py

--- Nanishop/settings.py
+++ Nanishop/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'Nanishop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # sử dụng PostgreSQL nếu có URL cơ sở dữ liệu
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
