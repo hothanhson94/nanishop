7cfda39d3ee410cafbf856171230d8e3338d3ee4
hothanhson94 <hothanhson1111@gmail.com>
2024-05-29 14:23:47 +0700

    Update build_files.sh
    Signed-off-by: hothanhson94 <hothanhson1111@gmail.com>

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@
 pip install -r requirements.txt
 
 # Run migrations
-python manage.py migrate
+python3 manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
