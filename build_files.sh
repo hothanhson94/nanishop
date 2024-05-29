6366c0ac41bba26472041778347994b8b9874287
hothanhson94 <hothanhson1111@gmail.com>
2024-05-29 13:56:02 +0700

    Delete build_files.sh
    Signed-off-by: hothanhson94 <hothanhson1111@gmail.com>

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# install dependencies
-pip install -r requirements.txt
-
-# run migrations
-python manage.py migrate
-
-# collect static files
-python manage.py collectstatic --noinput
