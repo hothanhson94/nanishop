42af0e5e01a4e4ba2ea578dac125294ec39c5352
hothanhson94 <hothanhson1111@gmail.com>
2024-05-29 15:17:07 +0700

    Update settings.py
    Signed-off-by: hothanhson94 <hothanhson1111@gmail.com>

--- Nanishop/settings.py
+++ Nanishop/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--&d8+veb4^r*c*
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['nanishop.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,12 +56,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'nanishop.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
