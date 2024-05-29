e478f4cb36416fcb91ebc85a63f942a763aaa2d7
hothanhson94 <hothanhson1111@gmail.com>
2024-05-29 13:48:37 +0700

    Create build_files.sh
    Signed-off-by: hothanhson94 <hothanhson1111@gmail.com>

---
+++ build_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# install dependencies
+pip install -r requirements.txt
+
+# run migrations
+python manage.py migrate
+
+# collect static files
+python manage.py collectstatic --noinput
